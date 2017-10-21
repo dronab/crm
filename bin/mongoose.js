c897148d4a3e0101f5526ac86d0b731230de300c
rusline18 <absaruslan90@mail.ru>
2017-10-21 10:10:32 +0300

    Обновил файлы

--- bin/mongoose.js
+++ bin/mongoose.js
@@ -7,4 +7,14 @@ mongoose.connect(config.get('mongoose:connection'), {
     useMongoClient: true,
 });
 
+mongoose.connection.on('error', (err) => {
+    console.error('Ошибка подключение к базе данных '+err);
+    console.error('Не запущен сервер MongoDb');
+    process.exit(2);
+});
+
+mongoose.connection.on('connected', () => {
+    console.info('Успешно подключена MongoDb');
+});
+
 module.exports = mongoose;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 let express = require('express');
 let router = express.Router();
+let User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/registration', (req, res, next) => {
+    res.render('registration', {
+        title: 'Регитрация',
+    })
+});
+
+router.post('/registration', (req, res) => {
+    let user = {
+        username: req.body.login,
+        password: req.body.password
+    };
+    let data = new User(user);
+    data.save().then((doc) => {
+        console.log('Сохранение объекта', doc);
+        res.redirect('/')
+    }).catch((err) => {
+        console.error(err.message);
+        res.render('registration', {
+            title: 'Регистрация',
+            message: err.message,
+        });
+    })
 });
 
 module.exports = router;
