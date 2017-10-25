540b38f7b107aeae147581919c9c81f03e45c50b
Ruslan Absalyamov <kazan1811>
2017-10-25 16:09:40 +0300

    Настроил все для passport-local-mongoose

--- models/user.js
+++ models/user.js
@@ -1,46 +1,26 @@
 let crypto = require('crypto'),
-    beautifyUnique = require('mongoose-beautiful-unique-validation'),
-    mongoose = require('../bin/mongoose'),
-    Schema = mongoose.Schema;
+	beautifyUnique = require('mongoose-beautiful-unique-validation'),
+	mongoose = require('../bin/mongoose'),
+	passportLocalMongoose = require('passport-local-mongoose'),
+	Schema = mongoose.Schema;
 
 let schema = new Schema({
-    username: {
-        type: String,
-        unique: 'Пользователь {VALUE} уже существует',
-        required: true
-    },
-    hashedPassword: {
-        type: String,
-        required: true
-    },
-    salt: {
-        type: String,
-        required: true
-    },
-    created: {
-        type: Date,
-        default: Date.now
-    }
+	username: {
+		type: String,
+		unique: 'Пользователь {VALUE} уже существует',
+		required: true
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	}
 });
 
 schema.plugin(beautifyUnique);
-
-schema.methods.encryptPassword = function (password) {
-    return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
-};
-
-schema.virtual('password')
-    .set(function (password) {
-        this._plainPassword = password;
-        this.salt = Math.random() + '';
-        this.hashedPassword = this.encryptPassword(password);
-    })
-    .get(function () {
-        return this._plainPassword;
-    });
-
-schema.methods.checkPassword = function (password) {
-    return this.encryptPassword(password) === this.hashedPassword;
-};
+schema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', schema);
