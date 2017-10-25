3ce09eb6c9e4be10129d9de63e08f18cd6114b83
Ruslan Absalyamov <kazan1811>
2017-10-25 16:08:25 +0300

    Создал файл passport.s

---
+++ boot/passport.js
@@ -0,0 +1,9 @@
+const passport = require('passport'),
+	User = require('../models/user'),
+	localPassport = require('passport-local').Strategy;
+
+passport.use(new localPassport(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+module.exports = localPassport;
