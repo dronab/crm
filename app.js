03d4c31edf45bc7db19298372f45550537fc6298
Ruslan Absalyamov <kazan1811>
2017-10-25 16:12:38 +0300

    изменил настройки сессии

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-let express = require('express'),
-    path = require('path'),
-    favicon = require('serve-favicon'),
-    logger = require('morgan'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    app = express();
+const express = require('express'),
+	path = require('path'),
+	config = require('./config'),
+	favicon = require('serve-favicon'),
+	logger = require('morgan'),
+	cookieParser = require('cookie-parser'),
+	bodyParser = require('body-parser'),
+	session = require('express-session'),
+	app = express();
 
 let index = require('./routes/index'),
-    users = require('./routes/users');
+	users = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +21,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+	secret: config.get('session:secret'),
+	resave: config.get('session:resave'),
+	saveUninitialized: config.get('session:saveUninitialized'),
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
@@ -26,20 +33,20 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	let err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
