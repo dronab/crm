a5d4eef479a82bb097f29a8c74ccfc254837a83e
Ruslan Absalyamov <kazan1811>
2017-10-25 16:10:24 +0300

    Настроил все к модули passport-local-mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,37 @@
-let express = require('express');
-let router = express.Router();
-let User = require('../models/user');
+const express = require('express'),
+	passport = require('passport'),
+	localPassport = require('../boot/passport'),
+	User = require('../models/user');
+
+const router = express.Router();
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Авторизация' });
+router.get('/', function(req, res) {
+	res.render('index', { title: 'Авторизация' });
 });
 
-router.get('/registration', (req, res, next) => {
-    res.render('registration', {
-        title: 'Регитрация',
-    })
+router.post('/', passport.authenticate('local', {
+	successRedirect: '/user',
+	failureRedirect: '/',
+}));
+
+router.get('/registration', (req, res) => {
+	res.render('registration', {
+	});
 });
 
 router.post('/registration', (req, res) => {
-    let user = {
-        username: req.body.username,
-        password: req.body.password
-    };
-    let data = new User(user);
-    data.save().then((doc) => {
-        console.log('Сохранение объекта', doc);
-        res.redirect('/')
-    }).catch((err) => {
-        console.error(err);
-        res.render('registration', {
-            title: 'Регистрация',
-            message: err.errors.username
-        });
-    })
+	User.register(new User({username: req.body.username}), req.body.password, (err, account) => {
+		if(err){
+			console.error(err.message);
+			console.error(err.account);
+			return res.render('registration', {account: account, message: err.message});
+		}
+		passport.authenticate('local')(req, res, function () {
+			res.redirect('/');
+		});
+	});
 });
 
 module.exports = router;
