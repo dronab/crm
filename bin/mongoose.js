0ec01a5cba030d2f34a2772e341228edd971dd63
Ruslan Absalyamov <kazan1811>
2017-10-25 16:07:56 +0300

    Согласнно eslint изменил

--- bin/mongoose.js
+++ bin/mongoose.js
@@ -1,20 +1,20 @@
 let mongoose = require('mongoose'),
-    config = require('../config');
+	config = require('../config');
 
 mongoose.Promise = require('bluebird');
 
 mongoose.connect(config.get('mongoose:connection'), {
-    useMongoClient: true,
+	useMongoClient: true,
 });
 
 mongoose.connection.on('error', (err) => {
-    console.error('Ошибка подключение к базе данных '+err);
-    console.error('Не запущен сервер MongoDb');
-    process.exit(2);
+	console.error('Ошибка подключение к базе данных '+err);
+	console.error('Не запущен сервер MongoDb');
+	process.exit(2);
 });
 
 mongoose.connection.on('connected', () => {
-    console.info('Успешно подключена MongoDb');
+	console.info('Успешно подключена MongoDb');
 });
 
 module.exports = mongoose;
