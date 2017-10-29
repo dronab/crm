c24bfb24e90873c39432b79650c377122778ece9
rusline18 <absaruslan90@mail.ru>
2017-10-29 19:55:57 +0300

    Переименовал вывод ошибок и удалил модуль mongoose-beautiful-unique-validation

--- models/user.js
+++ models/user.js
@@ -1,28 +1,26 @@
-let beautifyUnique = require('mongoose-beautiful-unique-validation'),
-	mongoose = require('../bin/mongoose'),
-	passportLocalMongoose = require('passport-local-mongoose'),
-	Schema = mongoose.Schema;
+let mongoose = require('../bin/mongoose'),
+    passportLocalMongoose = require('passport-local-mongoose'),
+    Schema = mongoose.Schema;
 
 let schema = new Schema({
-	username: {
-		type: String,
-		unique: 'Пользователь {VALUE} уже существует',
-		required: true
-	},
-	password: {
-		type: String,
-	},
-	created: {
-		type: Date,
-		default: Date.now
-	}
+    username: {
+        type: String,
+    },
+    password: {
+        type: String,
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
 });
 
-schema.plugin(beautifyUnique);
 schema.plugin(passportLocalMongoose, {
-	errorMessages: {
-		UserExistsError: 'Данный пользовательуже зарегестрировался'
-	}
+    errorMessages: {
+        UserExistsError: 'Данный пользователь уже зарегестрировался',
+        IncorrectPasswordError: 'Неправильный логин или пароль',
+        IncorrectUsernameError: 'Неправильный логин или пароль'
+    }
 });
 
 module.exports = mongoose.model('User', schema);
