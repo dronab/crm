b3bd65a3593a530186010ecfa800b85a41b4bdbe
Ruslan Absalyamov <absaruslan9@yandex.ru>
2017-11-13 15:18:38 +0300

    Обработка формы с файлами через ajax запрос

--- routes/create.js
+++ routes/create.js
@@ -1,11 +1,35 @@
 let express = require('express'),
     Order = require('../models/order'),
+    multiparty = require('multiparty'),
     router = express.Router();
 
 router.post('/zakaz', (req, res) => {
-   console.log(req.body);
-   if (!req.body) res.send(false);
-   let order = new Order(req);
+   let form = new multiparty.Form();
+    console.log(req.body);
+
+    form.on('error', (err) => {
+      console.log('Ошибка парсинга формы '+err);
+   });
+
+   form.on('part', (part) => {
+      // if(!part.filename){
+      //    console.log('Поле содержит текст '+ part.name);
+      //    part.resume();
+      // }
+      if(part.filename){
+         console.log('Поле содержит файл ' + part.name);
+          part.resume();
+      }
+   });
+
+    form.on('close', () => {
+       console.log('Зарузка завершена');
+    });
+
+   form.parse(req);
+   // console.log(req);
+   // if (!req.body) res.send(false);
+   // let order = new Order(req);
    res.send(true);
    // order.save()
    //     .then((order) => console.log('Сохранеине объекта '+order))
