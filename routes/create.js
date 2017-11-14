830b70b53715ee19de2b1d3bb56764a0b274896a
Ruslan Absalyamov <absaruslan9@yandex.ru>
2017-11-14 17:01:09 +0300

    Убрал лишние модули и переписал код под промиси

--- routes/create.js
+++ routes/create.js
@@ -1,41 +1,21 @@
 let express = require('express'),
     Order = require('../models/order'),
-    multiparty = require('multiparty'),
+    multipart = require('connect-multiparty'),
+    path = require('path'),
     router = express.Router();
 
-router.post('/zakaz', (req, res) => {
-    /** Приходит Post запроо от ajax и обрабатывается тут*/
-   let form = new multiparty.Form();
-    console.log(req.body);
-
-    form.on('error', (err) => {
-      console.log('Ошибка парсинга формы '+err);
-   });
-
-   form.on('part', (part) => {
-      // if(!part.filename){
-      //    console.log('Поле содержит текст '+ part.name);
-      //    part.resume();
-      // }
-      if(part.filename){
-         console.log('Поле содержит файл ' + part.name);
-          part.resume();
-      }
-   });
-
-   /** После успешной сохранение происходит close*/
-    form.on('close', () => {
-       console.log('Зарузка завершена');
+function orderSave(req) {
+    return new Promise((resolve, reject) => {
+        if(!req.body) reject(false);
+        let order = new Order(req.body);
+        order.save() ? resolve(true) : reject(false);
     });
+}
 
-   form.parse(req);
-   // console.log(req);
-   // if (!req.body) res.send(false);
-   // let order = new Order(req);
-   res.send(true);
-   // order.save()
-   //     .then((order) => console.log('Сохранеине объекта '+order))
-   //     .catch((err) => console.log('Произошла ошибка '+err));
+router.post('/order', multipart({uploadDir: path.join(__dirname, '../public/upload/')}), (req, res) => {
+   orderSave(req)
+       .then((orders) => res.send(orders))
+       .catch((err) => res.send(err));
 });
 
 module.exports = router;
