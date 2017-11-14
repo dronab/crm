e707d010c0c52abd204ec11b7e3bcb21853c1755
Ruslan Absalyamov <absaruslan9@yandex.ru>
2017-11-14 17:01:42 +0300

    Поменять дату

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 let express = require('express'),
 	Order = require('../models/order'),
+    moment = require('moment'),
 	router = express.Router();
 
 
@@ -7,6 +8,11 @@ let express = require('express'),
 router.get('/', (req, res) => {
 	Order.find()
         .then(orders => {
+            orders = orders.map((order) => {
+                order.time = moment(order.time).local('ru').format('DD MMMM YYYY');
+                console.log(order);
+                return order;
+            });
             res.render('user', {
                 username: req.user.username,
                 title: 'Главная страница',
