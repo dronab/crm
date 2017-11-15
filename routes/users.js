16565563f0e32ca0a1b2e55cadaa28327385fddf
Ruslan Absalyamov <absaruslan9@yandex.ru>
2017-11-15 16:26:59 +0300

    Преобразовал данные при выводе

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,23 @@ let express = require('express'),
     moment = require('moment'),
 	router = express.Router();
 
+moment.updateLocale('ru', {
+    months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
+});
 
 /* GET users listing. */
 router.get('/', (req, res) => {
-	Order.find()
+	Order.find().lean()
         .then(orders => {
             orders = orders.map((order) => {
-                order.time = moment(order.time).local('ru').format('DD MMMM YYYY');
-                console.log(order);
+                if (order.status === 0){
+                    order.status = 'Новый';
+                } else if (order.status === 1){
+                    order.status = 'Дизайнера';
+                } else if (order.status === 2){
+                    order.status = 'Мастера';
+                }
+                order.time = moment(order.time).local('ru').format('DD MMMM YYYY H:mm');
                 return order;
             });
             res.render('user', {
